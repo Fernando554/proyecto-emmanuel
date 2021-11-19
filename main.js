0a3c6cad9850627435378ee59912eb9c45d14b81
Mr Greg <gregsw123@gmail.com>
2021-11-19 16:21:59 -0600

    Main Modificado 2

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 let TotalC;
 let Montor;
 
+class Punto {
+    constructor ( x , y ){
+      this.x = x;
+      this.y = y;
+    }
+  
+    static distancia ( a , b) {
+      const dx = a.x - b.x;
+      const dy = a.y - b.y;
+  
+      return Math.sqrt ( dx * dx + dy * dy );
+    }
+  }
