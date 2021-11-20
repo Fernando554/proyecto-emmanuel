238eeeed05ee4c64df7bd5a75ea43ae3ccbf74de
Mr Greg <gregsw123@gmail.com>
2021-11-20 09:28:28 -0600

    Configuracion main

--- main.js
+++ main.js
@@ -1,3 +1,34 @@
 function datos(){
+  this.Nombre = "";
+  this.Apellido = "";
+  this.Usuario = "";
+
+  this.getNombre = function(){
+    return Nombre;
+  }
+
+  this.getApellido = function(){
+    return Apellido;
+  }
+
+  this.getUsuario = function(){
+    return Usuario;
+  }
+
+
+  this.setNombre = function(){
+    Nombre = Nob;
+  }
+
+  this.setApellido = function(){
+    Apellido = Ape;
+  }
+
+  this.setUsuario = function(){
+    Usuario = Usu;
+  }
+}
+
+function salida(){
   
 }
