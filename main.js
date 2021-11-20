7b1c9b56b3141e138973278b61980f225b711a67
fernando vargas bautista <fernando_1320114093@uptecamac.edu.mx>
2021-11-20 10:00:43 -0600

    cambios main 2

--- main.js
+++ main.js
@@ -31,4 +31,25 @@ function datos(){
 
 function salida(){
   
+  var obj= new datos();
+
+  var nomb= document.getElementById("nombre").value;
+  var apell= document.getElementById("apellido").value;
+  var usu= document.getElementById("usuario").value;
+
+  obj.setNombre(nomb);
+  obj.setApellido(apell);
+  obj.setUsuario(usu);
+
+  var nr1= obj.getNombre();
+  var nr2= obj.getApellido();
+  var nr3= obj.getUsuario();
+
+  document.write("Formularios con datos principales: ");
+
+  document.write("NOMBRE: "+nr1+"<br>");
+  document.write("APELLIDOS: "+nr2+"<br>");
+  document.write("USUARIO: "+nr3+"<br>");
+
+  
 }
