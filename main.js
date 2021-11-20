722fb400e85da51c318cc5e5d0d5c86a4c3083a8
Mr Greg <gregsw123@gmail.com>
2021-11-20 09:09:01 -0600

    Configuracion Main

--- main.js
+++ main.js
@@ -1,16 +1,3 @@
-let TotalC;
-let Montor;
-
-class Punto {
-    constructor ( x , y ){
-      this.x = x;
-      this.y = y;
-    }
+function datos(){
   
-    static distancia ( a , b) {
-      const dx = a.x - b.x;
-      const dy = a.y - b.y;
-  
-      return Math.sqrt ( dx * dx + dy * dy );
-    }
-  }
+}
